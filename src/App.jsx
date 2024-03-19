d932fd7a2273f69df8588e5ca9bd4c29d08b5cd3
Olga <minaeva9@gmail.com>
2024-03-19 17:00:34 -0400

    props added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import Navbar from "./Navbar";
 import Card from "./Card";
+import data from "./data";
+
 
 function App() {
+  const cards = data.map((card) => {
+    return (
+      <Card
+        key={card.key}
+        title={card.title}
+        location={card.location}
+        googleMapsUrl={card.googleMapsUrl}
+        startDate={card.startDate}
+        endDate={card.endDate}
+        description={card.description}
+        imageUrl={card.imageUrl}
+      />
+    );
+  });
+
   return (
     <>
       <Navbar />
-      <Card />
+      <section className="card--list">{cards}</section>
     </>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,27 +1,22 @@
 import "./Card.css";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="card">
       <div className="card--img--box">
-        <img
-          className="card--img"
-          src="https://source.unsplash.com/WLxQvbMyfas"
-          alt="image-name"
-        />
+        <img className="card--img" src={props.imageUrl} alt={props.title} />
       </div>
       <div className="card--info">
         <i className="fa-solid fa-location-dot card--location--icon"></i>
-        <h2 className="card--location">Japan</h2>
-        <a className="card--location--url" href="#">
+        <h2 className="card--location">{props.location}</h2>
+        <a className="card--location--url" href={props.googleMapsUrl}>
           View on Google Maps
         </a>
-        <h3 className="card--title">Mount Fuji</h3>
-        <p className="card--date">12 Jan, 2021 - 24 Jan, 2021</p>
-        <p className="card--description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-          voluptatibus.
+        <h3 className="card--title">{props.title}</h3>
+        <p className="card--date">
+          {props.startDate} - {props.endDate}
         </p>
+        <p className="card--description">{props.description}</p>
       </div>
     </div>
   );
