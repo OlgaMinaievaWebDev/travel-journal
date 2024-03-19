6ac833c286f88e3abecd688f060167e8a0f38d13
Olga <minaeva9@gmail.com>
2024-03-19 14:04:46 -0400

    navbar component added

---
+++ src/Navbar.jsx
@@ -0,0 +1,10 @@
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <i className="fa-solid fa-earth-europe nav--logo"></i>
+      <h4 className="nav--title">my travel journal</h4>
+    </nav>
+  );
+}
